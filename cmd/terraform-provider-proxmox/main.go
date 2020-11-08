e2c070f1b627a1e09aba682985effcc79d4981e4
fguiet@gmail.com <fguiet@gmail.com>
2020-11-08 15:13:12 +0100

    Fix

--- cmd/terraform-provider-proxmox/main.go
+++ cmd/terraform-provider-proxmox/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/Telmate/terraform-provider-proxmox/proxmox"
+	"github.com/fguiet/terraform-provider-proxmox/proxmox"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
